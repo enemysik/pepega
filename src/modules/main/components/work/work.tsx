1b29a96df4eddc1be75ebd95ff80b0a5cb768563
Anton <enemysik@gmail.com>
2020-03-22 12:24:18 +0700

    connect Work to redux

--- src/modules/main/components/work/work.tsx
+++ src/modules/main/components/work/work.tsx
@@ -3,56 +3,75 @@ import {IWork, IWorkTime} from '../../types';
 import {WorkTime} from './work-time';
 import {withChangeEvent} from '../../../../core/hoc/witch-change-event';
 import {fromHtml} from '../../../../core/hoc/from-html';
+import {RootState} from '../../../../reducers';
+import {
+  deleteWorkRemote,
+  updateWorkRemote,
+  updateTimeRangeRemote,
+  createTimeRangeRemote,
+  deleteTimeRangeRemote,
+  updateWork,
+} from '../../actions';
+import {connect, ConnectedProps} from 'react-redux';
 
 const Input = withChangeEvent(fromHtml('input'));
 const Textarea = withChangeEvent(fromHtml('textarea'));
-type Props = {
-  work: IWork;
-  onWorkChange: (work: IWork) => void;
-  onWorkChangeRemote: (work: IWork) => void;
-  onDelete: () => void;
-  onDeleteTime: (timeRangeId: number) => void;
-  onTimeCreate: () => void;
-  onTimeChange: (time: IWorkTime) => void;
-}
-export class Work extends Component<Props> {
-  render(): ReactNode {
-    const {work} = this.props;
 
-    return (
-      <div
-        className="card"
-        style={{backgroundColor: work.id === 0 ? 'grey' : 'white'}}
-      >
-        <div className="card-head">
-          <Input
-            className="form-control"
-            placeholder="Название"
-            onChange={(e) => this.props.onWorkChange({...this.props.work, name: e.currentTarget.value})}
-            valueChanged={(v) => this.props.onWorkChangeRemote({...this.props.work, name: v as string})}
-            value={work.name}
-          />
-          <button className="btn btn-outline-danger" onClick={() => this.props.onDelete()}>Delete</button>
-          <button className="btn btn-outline-success" onClick={() => this.props.onTimeCreate()}>Add time</button>
-        </div>
-        <div className="card-body">
-          <Textarea
-            className="form-control"
-            placeholder="Описание"
-            onChange={(e) => this.props.onWorkChange({...this.props.work, description: e.currentTarget.value})}
-            valueChanged={(v) => this.props.onWorkChangeRemote({...this.props.work, description: v as string})}
-            value={work.description || ''}
-          />
-        </div>
-        <div>
-          {work.times.slice().sort((a, b) => a.id - b.id).map((wt) => <WorkTime
-            key={wt.id}
-            workTime={wt}
-            onChange={(time) => this.props.onTimeChange(time)}
-            onDeleteTime={() => this.props.onDeleteTime(wt.id)}
-          />)}
-        </div>
-      </div>
-    );
-  }
+const mapState = (state: RootState) => ({
+  tasks: state.modules.main.tree.tasks,
+  selectedDate: state.modules.main.selectedDate,
+});
+const mapDispatch = {
+  onDelete: deleteWorkRemote,
+  onWorkChangeRemote: updateWorkRemote,
+  onWorkChange: updateWork,
+  onDeleteTime: deleteTimeRangeRemote,
+  onTimeCreate: createTimeRangeRemote,
+  onTimeChange: updateTimeRangeRemote,
+};
+const connector = connect(mapState, mapDispatch);
+type Props = ConnectedProps<typeof connector> & {
+  work: IWork;
 }
+export function Work({work, ...props}: Props) {
+  return (
+    <div
+      className="card"
+      style={{backgroundColor: work.id === 0 ? 'grey' : 'white'}}
+    >
+      <div className="card-head">
+        <Input
+          className="form-control"
+          placeholder="Название"
+          onChange={(e) => props.onWorkChange({...work, name: e.currentTarget.value})}
+          valueChanged={(v) => props.onWorkChangeRemote({...work, name: v as string})}
+          value={work.name}
+        />
+        <button className="btn btn-outline-danger" onClick={() => props.onDelete(work.id)}>Delete</button>
+        <button className="btn btn-outline-success" onClick={() => props.onTimeCreate({
+          workId: work.id,
+          time: props.selectedDate.toJSON(),
+        })}>Add time</button>
+      </div>
+      <div className="card-body">
+        <Textarea
+          className="form-control"
+          placeholder="Описание"
+          onChange={(e) => props.onWorkChange({...work, description: e.currentTarget.value})}
+          valueChanged={(v) => props.onWorkChangeRemote({...work, description: v as string})}
+          value={work.description || ''}
+        />
+      </div>
+      <div>
+        {work.times.slice().sort((a, b) => a.id - b.id).map((wt) => <WorkTime
+          key={wt.id}
+          workTime={wt}
+          onChange={(time) => props.onTimeChange({workId: work.id, time})}
+          onDeleteTime={() => props.onDeleteTime(work.id, wt.id)}
+        />)}
+      </div>
+    </div>
+  );
+}
+
+export default connector(Work);
--- src/modules/main/main.tsx
+++ src/modules/main/main.tsx
@@ -18,7 +18,7 @@ import {RootState} from '../../reducers';
 import {connect, ConnectedProps} from 'react-redux';
 import Tree from './components/tree/tree';
 import {WeekTabs} from './components/week-tabs/week-tabs';
-import {Work} from './components/work/work';
+import Work from './components/work/work';
 
 export class MainPage extends React.Component<IMainPageProps> {
   componentDidMount() {
@@ -32,15 +32,6 @@ export class MainPage extends React.Component<IMainPageProps> {
           <Work
             key={w.id}
             work={w}
-            onWorkChange={(w) => this.props.updateWork(w)}
-            onDelete={() => this.props.deleteWorkRemote(w.id)}
-            onWorkChangeRemote={(w) => this.props.updateWorkRemote(w)}
-            onDeleteTime={(tId) => this.props.deleteTimeRangeRemote(w.id, tId)}
-            onTimeCreate={() => this.props.createTimeRangeRemote({
-              workId: w.id,
-              time: this.props.selectedDate.toJSON(),
-            })}
-            onTimeChange={(time) => this.props.updateTimeRangeRemote({workId: w.id, time})}
           />);
     return (<main className="row">
       <div className="col-md-6">
